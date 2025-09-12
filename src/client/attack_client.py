5beb450f1a68123f2da8b4d4571b63f69b43de8f
id2714-ensai <felicien.rouvroy@eleve.ensai.fr>
2025-09-12 14:55:32 +0000

    TP3

--- src/client/attack_client.py
+++ src/client/attack_client.py
@@ -36,14 +36,24 @@ class AttackClient(metaclass=Singleton):
         # Check if the request is ok
         if req.status_code == 200:
             raw_attack = req.json()
-
             print("Réponse JSON obtenue :\n" + json.dumps(raw_attack, indent=2) + "\n")
-
-            # TODO
-            #   create an attack using the data contained in the json
-            #   see class AttackFactory to do this
-
+            attack_factory = AttackFactory()
+            attack = attack_factory.instantiate_attack(type = req["attack_type"], 
+            id = req["id"], power = req["power"], name = req["name"], description = req["description"],
+            accuracy= req["accuracy"], element = req["element"])
         return attack
+
+    def get_all_attack(self):
+        attack_factory = AttackFactory()
+        attack_list =[]
+        url = f"{self.__HOST}{END_POINT}"
+        req = requests.get(url)
+        raw_pok = req.json()
+        for pok in raw_pok:
+            attack_list.append(attack_factory.instantiate_attack(type = pok["attack_type"], 
+            id = pok["id"], power = pok["power"], name = pok["name"], description = pok["description"],
+            accuracy= pok["accuracy"], element = pok["element"]))
+        return attack_list
 
 
 # Execute Code When the File Runs as a Script
